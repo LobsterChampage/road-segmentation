b5a9a7f6aca16817a2ef97d98a040e77e724fc9c
TeriMeri <fredrik.haegermark@epfl.ch>
2021-12-03 20:15:46 +0100

    kjøyr

--- functions.py
+++ functions.py
@@ -30,18 +30,8 @@ def create_rotate_img():
         t_gt = Image.open(train_gt_root_dir + train_gt_img_file[i])
 
         for j in range(0, 360, 90):
-            if j == 0:
-                rot_t = t.transpose(Image.ROTATE_0)
-                rot_t_gt = t_gt.transpose(Image.ROTATE_0)
-            elif j == 90:
-                rot_t = t.transpose(Image.ROTATE_90)
-                rot_t_gt = t_gt.transpose(Image.ROTATE_90)
-            elif j == 180:
-                rot_t = t.transpose(Image.ROTATE_180)
-                rot_t_gt = t_gt.transpose(Image.ROTATE_180)
-            else:
-                rot_t = t.transpose(Image.ROTATE_270)
-                rot_t_gt = t_gt.transpose(Image.ROTATE_270)
+            rot_t = t.transpose(Image.rotate(j))
+            rot_t_gt = t_gt.transpose(Image.rotate(j))
 
             rot_t = rot_t.save(f"{image_path}/rot{i}_{j}.png")
             rot_t_gt = rot_t_gt.save(f"{image_path_gt}/rot{i}_{j}.png")
